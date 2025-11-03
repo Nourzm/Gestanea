1c6f52fd98aecf1edf0fa8f861094cb0f4520c8f
Maroua <157848712+Maroua0618@users.noreply.github.com>
2025-11-03 15:46:30 +0100

    Implement market page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:pregnancy_baby_app/features/marketplace/presentation/pages/marketplace_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pregnancy & Baby App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MarketplacePage(),
+    );
+  }
 }
