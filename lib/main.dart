a88173a84364fbe017516e33105858140c30ceeb
Maroua <157848712+Maroua0618@users.noreply.github.com>
2025-12-29 21:24:15 +0100

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 final RouteObserver<ModalRoute<void>> routeObserver =
     RouteObserver<ModalRoute<void>>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
